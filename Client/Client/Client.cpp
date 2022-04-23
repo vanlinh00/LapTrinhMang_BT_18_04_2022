f439eb2a91b3b8dda0cffeac8c08d0ba4d9f170a
vanlinh00 <82046592+vanlinh00@users.noreply.github.com>
2022-04-24 02:13:15 +0700

    adding files

---
+++ Client/Client/Client.cpp
@@ -0,0 +1,135 @@
+﻿
+#include <stdio.h>
+#include <WinSock2.h>
+#include <wchar.h>
+#include <comdef.h>
+
+#pragma comment(lib, "ws2_32")
+#pragma warning(disable:4996)
+
+int main()
+{
+    WSADATA wsa;
+    WSAStartup(MAKEWORD(2, 2), &wsa);
+
+    SOCKET server = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    SOCKADDR_IN addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr("192.168.0.103");
+    addr.sin_port = htons(8000);
+
+    system("pause");
+
+    int ret = connect(server, (SOCKADDR*)&addr, sizeof(addr));
+    if (ret == SOCKET_ERROR)
+    {
+        ret = WSAGetLastError();
+        printf("Ket noi khong thanh cong - %d\n", ret);
+        return 1;
+    }
+    
+    // tên máy tính
+    wchar_t computerName[MAX_COMPUTERNAME_LENGTH + 1];
+    DWORD size = sizeof(computerName) / sizeof(computerName[0]);
+
+    int r = GetComputerNameW(computerName, &size);
+
+    if (r == 0) {
+        wprintf(L"Failed to get computer name %ld", GetLastError());
+        return 1;
+    }
+
+  wprintf(L"Computer name: %ls\n", computerName);
+
+    _bstr_t b(computerName);
+    const char* msg = b;
+
+    send(server, msg, strlen(msg), 0);
+
+
+  
+  //  GetLogicalDriveStringsA() - Trả về danh sách ký tự ổ đĩa của máy tính
+   
+    wchar_t LogicalDrives[MAX_PATH] = { 0 };
+    DWORD Lgd = GetLogicalDriveStringsW(MAX_PATH, LogicalDrives);
+
+    if (Lgd == 0) {
+        wprintf(L"Failed to get drive names %ld", GetLastError());
+        return 1;
+    }
+
+    if (Lgd > 0 && Lgd <= MAX_PATH) {
+
+        wchar_t* SingleDrive = LogicalDrives;
+
+        while (*SingleDrive) {
+
+            wprintf(L"%ls\n", SingleDrive);
+
+            _bstr_t b(SingleDrive);
+            const char* msg = b;
+            send(server, msg, strlen(msg), 0);
+
+
+            SingleDrive += wcslen(SingleDrive) + 1;
+
+     
+        }
+    }
+    
+    
+
+
+ 
+// GetDiskFreeSpaceA() - Trả về kích thước của ổ đĩa
+
+    unsigned __int64 freeCall,
+        total,
+        free;
+
+    int DF = GetDiskFreeSpaceExW(L"C:\\", (PULARGE_INTEGER)&freeCall,
+        (PULARGE_INTEGER)&total, (PULARGE_INTEGER)&free);
+
+    if (DF == 0) {
+
+        wprintf(L"Failed to get free disk space %ld", GetLastError());
+        return 1;
+    }
+
+ wprintf(L"Available space to caller: %I64u MB\n", freeCall / (1024 * 1024));
+
+    _bstr_t b0(freeCall);
+    const char* msg0 = b0;
+    send(server, msg0, strlen(msg0), 0);
+
+ wprintf(L"Total space: %I64u MB\n", total / (1024 * 1024));
+
+
+    _bstr_t b1(total);
+    const char* msg1 = b1;
+    send(server, msg1, strlen(msg1), 0);
+
+  wprintf(L"Free space on drive: %I64u MB\n", free / (1024 * 1024));
+
+    _bstr_t b2(free);
+    const char* msg2 = b2;
+    send(server, msg2, strlen(msg2), 0);
+
+
+     // Nhan du lieu tu server va in ra man hinh
+    char buf[256];
+    while (1)
+    {
+        ret = recv(server, buf, sizeof(buf), 0);
+        if (ret <= 0)
+            break;
+
+        // Them ky tu ket thuc xau va in ra man hinh
+        buf[ret] = 0;
+        printf("Du lieu tu server: %s\n", buf);
+    }
+
+    closesocket(server);
+    WSACleanup();
+}
---
+++ Server/Server/Server.cpp
@@ -0,0 +1,74 @@
+﻿// câu lệnh tạo server bằng netcat nc64.exe 192.168.0.103 8000
+// VD3.cpp : Tao server nhan cau chao tu client, sau do doc du lieu tu ban phim roi gui sang client.
+
+#include <stdio.h>
+#include <winsock2.h>
+
+#pragma comment(lib, "ws2_32")
+
+int main()
+{
+    // Khoi tao thu vien
+    WSADATA wsa;
+    WSAStartup(MAKEWORD(2, 2), &wsa);
+
+    // Tao socket
+    SOCKET listener = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    // Khai bao dia chi server
+    SOCKADDR_IN addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(8000);
+
+    // Gan cau truc dia chi voi socket
+    bind(listener, (SOCKADDR*)&addr, sizeof(addr));
+
+    // Chuyen sang trang thai cho ket noi
+    listen(listener, 5);
+
+    // Cho ket noi moi
+    SOCKET client = accept(listener, NULL, NULL);
+
+
+  
+
+    // Lien tuc doc du lieu tu ban phim va gui sang client
+    // Nhan du lieu tu client
+    char buf[256];
+    while (1)
+    {
+
+       
+        int ret = recv(client, buf, sizeof(buf), 0);
+
+        // Them ky tu ket thuc xau va in ra man hinh
+        buf[ret] = 0;
+
+        ///  int check = strcmp(buf, "");
+
+        printf("Du lieu tu client: %s\n", buf);
+
+            
+   /*
+
+            printf("Nhap xau ky tu: ");
+            fgets(buf, sizeof(buf), stdin);
+
+            if (strncmp(buf, "exit", 4) == 0)
+                break;
+
+            send(client, buf, strlen(buf), 0);
+       */
+     
+
+
+
+
+    }
+
+    // Dong ket noi va giai phong thu vien
+    closesocket(client);
+    closesocket(listener);
+    WSACleanup();
+}
